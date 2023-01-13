ca8265b0cf9537f8356aa2b79dd645fc978eb96a
SejinOH <39869184+designerSejinOH@users.noreply.github.com>
2023-01-13 13:39:07 +0900

    add div in P5Canvas

--- src/components/canvas/P5Canvas.tsx
+++ src/components/canvas/P5Canvas.tsx
@@ -2,21 +2,24 @@ import React from 'react'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper'
 
 function sketch(p5: P5CanvasInstance) {
-  p5.setup = () => p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL)
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight)
+    p5.background(100)
+  }
   p5.windowResized = () => p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
   p5.draw = () => {
-    p5.orbitControl()
-    p5.background(250)
-    p5.normalMaterial()
     p5.push()
-    p5.rotateZ(p5.frameCount * 0.01)
-    p5.rotateX(p5.frameCount * 0.01)
-    p5.rotateY(p5.frameCount * 0.01)
-    p5.plane(100)
+    p5.fill(100, 10)
+    p5.rect(0, 0, p5.width, p5.height)
     p5.pop()
+    p5.ellipse(p5.mouseX, p5.mouseY, 100, 100)
   }
 }
 
 export default function P5Canvas() {
-  return <ReactP5Wrapper sketch={sketch} />
+  return (
+    <div className='overflow-hidden'>
+      <ReactP5Wrapper sketch={sketch} />
+    </div>
+  )
 }
